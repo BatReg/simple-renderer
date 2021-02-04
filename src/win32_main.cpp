75f806dc160f6f6e501b933bc008c1673e160a41
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2021-02-05 01:09:08 +0200

    Fix WinMain declaration

--- src/win32_main.cpp
+++ src/win32_main.cpp
@@ -2,7 +2,12 @@
 
 #include "platform/window.h"
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+int CALLBACK WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine,
+    _In_ int nShowCmd
+)
 {
     Platform::Window wnd{};
     wnd.Init("Super Awesome Title");
