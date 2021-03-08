62a69b3347e39eba353a2742e50144c778a3c58a
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2021-03-08 22:13:08 +0200

    Add vulkan instance initialization and destroying

--- src/renderer/vulkan/vk_renderer.cpp
+++ src/renderer/vulkan/vk_renderer.cpp
@@ -1,21 +1,70 @@
 #include "renderer/renderer.h"
 
+#include <vulkan/vulkan.h>
+#include "external/VkBootstrap.h"
+
 #include <iostream>
 
-struct Renderer::Material
+namespace Renderer
 {
-};
+    class Renderer::NativeRenderer
+    {
+    public:
+        void Init();
+        void Destroy();
 
-struct Renderer::Mesh
-{
-};
+    private:
+        void InitVulkan();
 
-void Renderer::Renderer::Init()
-{
-    std::cout << "Renderer initialized" << std::endl;
-}
+        bool m_isInitalized;
 
-void Renderer::Renderer::Destroy()
-{
-    std::cout << "Renderer destroyed" << std::endl;
+        VkInstance                  m_Instance;
+        VkDebugUtilsMessengerEXT    m_DebugMessenger;
+    };
+
+    Renderer::Renderer() : m_NativeRenderer(std::make_unique<NativeRenderer>()) {}
+
+    Renderer::~Renderer()
+    {
+        m_NativeRenderer.reset();
+    }
+
+    void Renderer::Init()
+    {
+        m_NativeRenderer->Init();
+    }
+
+    void Renderer::Destroy()
+    {
+        m_NativeRenderer->Destroy();
+    }
+
+    void Renderer::NativeRenderer::Init()
+    {
+        InitVulkan();
+        std::cout << "Vulkan Renderer initialized" << std::endl;
+    }
+
+    void Renderer::NativeRenderer::Destroy()
+    {
+        vkb::destroy_debug_utils_messenger(m_Instance, m_DebugMessenger, nullptr);
+        vkDestroyInstance(m_Instance, nullptr);
+        std::cout << "Vulkan Renderer destroyed" << std::endl;
+    }
+
+    void Renderer::NativeRenderer::InitVulkan()
+    {
+        vkb::InstanceBuilder instanceBuilder;
+        auto instanceRet = instanceBuilder
+            .set_app_name("Test")
+            .request_validation_layers(true)
+            .require_api_version(1, 1, 0)
+            .use_default_debug_messenger()
+            .build();
+        
+        vkb::Instance vkbInstance = instanceRet.value();
+
+        m_Instance = vkbInstance.instance;
+        m_DebugMessenger = vkbInstance.debug_messenger;
+    }
 }
