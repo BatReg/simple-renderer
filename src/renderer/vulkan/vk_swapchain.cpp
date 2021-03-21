7f868fad675b0ed7dce415fa2a199885b0de55fb
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2021-03-21 22:20:39 +0200

    Add namespace. Add empty swapchain implementation.

--- src/renderer/vulkan/vk_swapchain.cpp
+++ src/renderer/vulkan/vk_swapchain.cpp
@@ -1,1 +1,20 @@
 #include "vk_swapchain.h"
+
+namespace Renderer::Vulkan
+{
+    SwapChain::SwapChain(VkInstance instance, VkPhysicalDevice physicalDevice, VkDevice device) noexcept
+    {
+    }
+
+    void SwapChain::CreateSurface(const Platform::Window& window) noexcept
+    {
+    }
+
+    void SwapChain::CreateSwapchain(const uint32_t& width, const uint32_t& height, bool vSync) noexcept
+    {
+    }
+
+    void SwapChain::Dispose() noexcept
+    {
+    }
+}
