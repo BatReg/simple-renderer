9b164fb9fc1c02522d3939efdc8c37610208ac0a
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2021-03-21 22:20:39 +0200

    Add namespace. Add empty swapchain implementation.

--- src/renderer/vulkan/vk_core.h
+++ src/renderer/vulkan/vk_core.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cassert>
+
 #define VK_CHECK_RESULT(f)                                                                                       \
 {                                                                                                                \
     VkResult res = (f);                                                                                          \
--- src/renderer/vulkan/vk_swapchain.h
+++ src/renderer/vulkan/vk_swapchain.h
@@ -5,41 +5,44 @@
 
 #include <vector>
 
-struct SwapChainBuffer
+namespace Renderer::Vulkan
 {
-    VkImage     image;
-    VkImageView view;
-};
-
-class VulkanSwapchain
-{
-public:
-    VulkanSwapchain(VkInstance instance, VkPhysicalDevice physicalDevice, VkDevice device) noexcept;
-
-    VulkanSwapchain() = delete;
-    VulkanSwapchain(const VulkanSwapchain&) = delete;
-    VulkanSwapchain& operator=(const VulkanSwapchain&) = delete;
-
-    void CreateSurface(const Platform::Window& window) noexcept;
-    void CreateSwapchain(const uint32_t& width, const uint32_t& height, bool vSync = false) noexcept;
-    void Dispose() noexcept;
-
-    VkSurfaceKHR GetSurface() const noexcept
+    struct SwapChainBuffer
     {
-        return m_Surface;
-    }
+        VkImage     image;
+        VkImageView view;
+    };
 
-private:
-    VkInstance                      m_Instance;
-    VkDevice                        m_Device;
-    VkPhysicalDevice                m_PhysicalDevice;
-    VkSurfaceKHR                    m_Surface;
-    VkFormat                        m_ColorFormat;
-    VkColorSpaceKHR                 m_ColorSpace;
-    VkSwapchainKHR                  m_SwapChain = VK_NULL_HANDLE;
+    class SwapChain
+    {
+    public:
+        SwapChain(VkInstance instance, VkPhysicalDevice physicalDevice, VkDevice device) noexcept;
 
-    uint32_t                        m_ImageCount;
-    uint32_t                        m_QueueNodeIndex = UINT32_MAX;
-    std::vector<VkImage>            m_Images;
-    std::vector<SwapChainBuffer>    m_Buffers;
-};
+        SwapChain() = delete;
+        SwapChain(const SwapChain&) = delete;
+        SwapChain& operator=(const SwapChain&) = delete;
+
+        void CreateSurface(const Platform::Window& window) noexcept;
+        void CreateSwapchain(const uint32_t& width, const uint32_t& height, bool vSync = false) noexcept;
+        void Dispose() noexcept;
+
+        VkSurfaceKHR GetSurface() const noexcept
+        {
+            return m_Surface;
+        }
+
+    private:
+        VkInstance                      m_Instance;
+        VkDevice                        m_Device;
+        VkPhysicalDevice                m_PhysicalDevice;
+        VkSurfaceKHR                    m_Surface;
+        VkFormat                        m_ColorFormat;
+        VkColorSpaceKHR                 m_ColorSpace;
+        VkSwapchainKHR                  m_SwapChain = VK_NULL_HANDLE;
+
+        uint32_t                        m_ImageCount;
+        uint32_t                        m_QueueNodeIndex = UINT32_MAX;
+        std::vector<VkImage>            m_Images;
+        std::vector<SwapChainBuffer>    m_Buffers;
+    };
+}
