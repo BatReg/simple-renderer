379a85445792c40e9c976facfe184bf142feefeb
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2021-04-05 00:13:25 +0300

    Implement simple command buffer creation

--- src/renderer/vulkan/vk_renderer.cpp
+++ src/renderer/vulkan/vk_renderer.cpp
@@ -22,8 +22,9 @@ namespace Renderer
     private:
         void InitVulkan() noexcept;
         void InitSwapchain() noexcept;
+        void InitCommands() noexcept;
 
-        bool                        m_isInitalized{ false };
+        bool                        m_IsInitalized{ false };
 
         const Platform::Window&     m_Window;
 
@@ -32,8 +33,13 @@ namespace Renderer
         VkDevice                    m_Device;
         VkPhysicalDevice            m_PhysicalDevice;
         VkSurfaceKHR                m_Surface;
+        VkQueue                     m_GraphicsQueue;
+        uint32_t                    m_GraphicsQueueFamily;
 
         Vulkan::SwapChain           m_SwapChain{};
+
+        VkCommandPool               m_CommandPool;
+        VkCommandBuffer             m_CommandBuffer;
     };
 
     Renderer::Renderer(const Platform::Window& window) noexcept :
@@ -62,11 +68,21 @@ namespace Renderer
     {
         InitVulkan();
         InitSwapchain();
+        InitCommands();
+
+        m_IsInitalized = true;
         std::cout << "Vulkan Renderer initialized" << std::endl;
     }
 
     void Renderer::NativeRenderer::Destroy() noexcept
     {
+        if(!m_IsInitalized)
+        {
+            return;
+        }
+
+        vkDestroyCommandPool(m_Device, m_CommandPool, nullptr);
+
         m_SwapChain.Destroy();
 
         vkDestroyDevice(m_Device, nullptr);
@@ -87,8 +103,8 @@ namespace Renderer
 
         vkb::Instance vkbInstance = instanceRet.value();
 
-        m_Instance = vkbInstance.instance;
-        m_DebugMessenger = vkbInstance.debug_messenger;
+        m_Instance          = vkbInstance.instance;
+        m_DebugMessenger    = vkbInstance.debug_messenger;
 
         VK_CHECK_RESULT(Platform::Vulkan::CreateSurface(m_Window, m_Instance, &m_Surface));
 
@@ -104,21 +120,43 @@ namespace Renderer
         vkb::DeviceBuilder deviceBuilder{ physicalDevice };
         vkb::Device vkbDevice = deviceBuilder.build().value();
 
-        m_PhysicalDevice = physicalDevice.physical_device;
-        m_Device = vkbDevice.device;
+        m_PhysicalDevice        = physicalDevice.physical_device;
+        m_Device                = vkbDevice.device;
+        m_GraphicsQueue         = vkbDevice.get_queue(vkb::QueueType::graphics).value();
+        m_GraphicsQueueFamily   = vkbDevice.get_queue_index(vkb::QueueType::graphics).value();
     }
 
     void Renderer::NativeRenderer::InitSwapchain() noexcept
     {
         Vulkan::SwapChainInitInfo swapChainInitInfo{};
-        swapChainInitInfo.instance = m_Instance;
-        swapChainInitInfo.physicalDevice = m_PhysicalDevice;
-        swapChainInitInfo.device = m_Device;
-        swapChainInitInfo.surface = m_Surface;
-        
+        swapChainInitInfo.instance          = m_Instance;
+        swapChainInitInfo.physicalDevice    = m_PhysicalDevice;
+        swapChainInitInfo.device            = m_Device;
+        swapChainInitInfo.surface           = m_Surface;
+
         m_SwapChain.Init(swapChainInitInfo);
 
         // TODO (Nikita): implement passing window's width and height
         m_SwapChain.CreateSwapChain(800, 600, true);
+    }
+
+    void Renderer::NativeRenderer::InitCommands() noexcept
+    {
+        VkCommandPoolCreateInfo commandPoolInfo{};
+        commandPoolInfo.sType               = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+        commandPoolInfo.pNext               = nullptr;
+        commandPoolInfo.queueFamilyIndex    = m_GraphicsQueueFamily;
+        commandPoolInfo.flags               = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+
+        VK_CHECK_RESULT(vkCreateCommandPool(m_Device, &commandPoolInfo, nullptr, &m_CommandPool));
+
+        VkCommandBufferAllocateInfo cmdAllocInfo{};
+        cmdAllocInfo.sType              = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        cmdAllocInfo.pNext              = nullptr;
+        cmdAllocInfo.commandPool        = m_CommandPool;
+        cmdAllocInfo.commandBufferCount = 1;
+        cmdAllocInfo.level              = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+
+        VK_CHECK_RESULT(vkAllocateCommandBuffers(m_Device, &cmdAllocInfo, &m_CommandBuffer));
     }
 }
